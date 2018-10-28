20922e934d1482d530beb987fd57c5adae051e20
albplanas <albplanas0904@gmail.com>
2018-10-28 14:19:40 -0500

    Just a test

--- src/js/components/container/Home/Top_Bar.js
+++ src/js/components/container/Home/Top_Bar.js
@@ -59,9 +59,9 @@ class TopBar extends Component {
         }
         
         if(this.state.Log_active==="LOGIN"){
+            console.log("Test")
             axios.post("/login", user)
                 .then((result) => {
-                     console.log("HIIIII")
                      result.data.login ? this.props.Login( result.data.login, result.data.profile) : this.props.Login( result.data.login  , { username   : "", src        : "", email      : "", status     : "", location   : ""})
                      this.props.Papers(result.data.papers)
 
